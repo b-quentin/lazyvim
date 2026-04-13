3c44a92b9c8d3015b67b985e9f2cd0f064818996
bquentin <bquentin@protonmail.com>
2026-04-13 21:04:59 +0200

    add overseer

---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/overseer.nvim",
+  cmd = {
+    "OverseerRun",
+    "OverseerToggle",
+    "OverseerQuickAction",
+  },
+
+  opts = {
+    strategy = "toggleterm",
+    templates = { "builtin" },
+  },
+
+  keys = {
+    { "<leader>rr", "<cmd>OverseerRun<cr>", desc = "Run task" },
+    { "<leader>rl", "<cmd>OverseerToggle<cr>", desc = "Task list" },
+    { "<leader>ra", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
+  },
+}
