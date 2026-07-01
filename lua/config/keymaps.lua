72d1ce25aa494ca22c455acd3cce3543ff62b90d
Quentin Besse <quentin.besse.ext@orange.com>
2026-07-01 10:01:50 +0200

    feat(config): add custom keymaps for git conflicts and paths

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,30 @@ vim.api.nvim_create_user_command("LspRestart", function()
   end
   vim.cmd("edit")
 end, {})
+
+vim.keymap.set("n", "<leader>p", function()
+  print(vim.fn.expand("%:p"))
+end, { desc = "custom Show full file path" })
+
+vim.keymap.set("n", "<leader>gc", function()
+  local files = vim.fn.systemlist("git diff --name-only --diff-filter=U")
+  local items = {}
+
+  for _, file in ipairs(files) do
+    table.insert(items, {
+      filename = file,
+      lnum = 1,
+      text = "git conflict",
+    })
+  end
+
+  vim.fn.setqflist(items)
+  vim.cmd("copen")
+end, { desc = "custom Git: list conflicts (quickfix)" })
+
+vim.keymap.set("n", "<leader>cn", ":cnext<CR>", { silent = true, desc = "custom Quickfix next" })
+vim.keymap.set("n", "<leader>cp", ":cprev<CR>", { silent = true, desc = "custom Quickfix prev" })
+
+vim.keymap.set("n", "<leader>xn", "]x", { remap = true, silent = true, desc = "custom Git conflict next" })
+
+vim.keymap.set("n", "<leader>xp", "[x", { remap = true, silent = true, desc = "custom Git conflict prev" })
