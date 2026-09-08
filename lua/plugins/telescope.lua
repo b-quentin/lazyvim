1770f2f86a5e4b766205e068c33542dbd87a7b7c
Quentin Besse <quentin.besse.ext@orange.com>
2026-09-08 10:08:15 +0200

    feat(telescope): config for hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,19 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    opts = {
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+        },
+      },
     },
   },
 }
